8e24d7d223316000e45208d1340f24617fd4bd15
forynski <65871510+forynski@users.noreply.github.com>
2020-11-01 21:53:50 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 package cinema;
 
+import java.util.Arrays;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+        String[][] myArray = {
+                {" ", "1", "2", "3", "4", "5", "6", "7", "8"},
+                {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"2", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"3", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"4", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"5", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"6", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"7", "S", "S", "S", "S", "S", "S", "S", "S"}
+        };
+
+        for (int i = 0; i < myArray.length; i++)
+            System.out.println(Arrays.toString(myArray[i]));
     }
 }
