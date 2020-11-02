77930a63fd84375d1ffcef311141f08d1ede1fc3
forynski <65871510+forynski@users.noreply.github.com>
2020-11-02 09:55:26 +0000

    stage 1/5 done

--- src/Cinema.java
+++ src/Cinema.java
@@ -6,6 +6,7 @@ public class Cinema {
 
     public static void main(String[] args) {
         String[][] myArray = {
+                {"Cinema:"},
                 {" ", "1", "2", "3", "4", "5", "6", "7", "8"},
                 {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
                 {"2", "S", "S", "S", "S", "S", "S", "S", "S"},
@@ -17,6 +18,9 @@ public class Cinema {
         };
 
         for (int i = 0; i < myArray.length; i++)
-            System.out.println(Arrays.toString(myArray[i]));
+            System.out.println(Arrays.toString(myArray[i])
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", ""));
     }
 }
